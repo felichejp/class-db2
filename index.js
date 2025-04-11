3f6db519280bbe97f977d69041a058dd2479379c
Felix Jimenez <felichefie@gmail.com>
2025-04-11 08:36:14 -0600

    first commit to AWS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello DB2 class commit!!!')
+  res.send('Hello DB2 class first commit!!!')
 })
 
 app.listen(port, () => {
