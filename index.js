2007c3d1b327bd72db13505da01f512f28e4fe8f
Jualiche <SkettK@gmail.com>
2025-05-08 13:17:50 -0600

    Commit de Jualiche

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000
 
 app.get('/', (req, res) => {
-  res.send("Hola Didocapi(Diego Calderon)")
+  res.send("Hola buenas tardes, va a querer pistaches? (Juan Uriel)")
 })
 
 app.listen(port, () => {
