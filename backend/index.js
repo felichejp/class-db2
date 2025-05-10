73db561f674c1ee9e3650051b4e67e5087fd2d0c
Felix Jimenez <felichefie@gmail.com>
2025-05-10 11:04:07 -0600

    WIP on felix/first_back

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,50 @@
 const express = require('express');
-const session = require('express-session');
+const dotenv = require('dotenv');
+const pg = require('pg');
+
+dotenv.config();
 
 const app = express()
-const port = 8000
+const port = 9000
+app.use(express.json())
 
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 5,
-    httpOnly: true,
-    secure: false
+
+const client = new pg.Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE
+})
+
+app.post('/auth', (req, res) => {
+  const { username, password } = req.body;
+  if (!client.connected) {
+    client.connect();
   }
-}))
-
-app.get('/', (req, res) => {
-  if (req.session.userid) {
-    res.send("Fabian first PR:)")
+  if (username === "admin" && password === "admin") {
+    res.send("Login successful");
   } else {
-    res.send("Unauthorized");
+    res.send("Login failed");
   }
 })
 
-app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.send("Logout");
+app.post('/isUserAuth', async (req, res) => {
+    const { username, password } = req.body;
+    if (!client.connected) {
+      client.connect();
+    }
+    const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length > 0) {
+      if (result.rows[0].password === password) {
+        res.send("User authenticated");
+      } else {
+        res.send("User not authenticated");
+      }
+    } else {
+      res.send("User not authenticated");
+    }
 })
-
-app.get('/login', (req, res) => {
-  req.session.userid = 1234;
-  res.send("Login");
-})
-
-app.get('/private', (req, res) => {
-  if (!req.session.username) {
-    res.send("Unauthorized");
-  }
-  res.send("Private");
-})
-
-app.get('/wallet', (req, res) => {
-if (!req.session.userid) {
-    res.send("Unauthorized");
-  }
-  res.send("Wallet amount 1000");
-})
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
