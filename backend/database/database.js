eafaf255de795a70f346c51857f711c177479825
Felix Jimenez <felichefie@gmail.com>
2025-05-14 08:06:45 -0600

    test backend 1

---
+++ backend/database/database.js
@@ -0,0 +1,29 @@
+const { Client } = require('pg') // Importar Client de pg
+require('dotenv').config() // Importar configuración de dotenv
+
+// Función para conectar a la base de datos
+async function connect () {
+  // Crear instancia de Client con la configuración de dotenv
+  const client = new Client({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
+  });
+  await client.connect() // Conectar a la base de datos
+  console.log('Conneted to database') // Mostrar mensaje en consola
+  return client // Retornar cliente
+}
+
+// Función para realizar consultas
+async function query(query, client, params = []) {
+  const res = await client.query(query, params) // Realizar consulta
+  return res.rows // Retornar filas de la respuesta
+}
+
+// Exportar funciones
+module.exports = {
+  connect,
+  query
+}
--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const pg = require('pg');
-
+const { connect, query } = require('./database/database');
 dotenv.config();
 
 const app = express()
 const port = 9000
 app.use(express.json())
 
-const client = new pg.Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE
-})
-
-app.post('/auth', (req, res) => {
+app.post('/auth', async(req, res) => {
   const { username, password } = req.body;
-  if (!client.connected) {
-    client.connect();
+  if( !username || !password ) {
+    res.status(400).send("Username and password are required");
+    return;
   }
-  if (username === "admin" && password === "admin") {
-    res.send("Login successful");
+  const client = await connect();
+  const result = await query(client, 'SELECT * FROM users WHERE username = $1', [username]);
+  if (result.rows.length > 0 && result.row[0].isPassOk) {
+    res.send("User authenticated");
   } else {
-    res.send("Login failed");
+    res.send("User not authenticated");
   }
-})
-
-app.post('/isUserAuth', async (req, res) => {
-    const { username, password } = req.body;
-    if (!client.connected) {
-      client.connect();
-    }
-    const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
-    if (result.rows.length > 0) {
-      if (result.rows[0].password === password) {
-        res.send("User authenticated");
-      } else {
-        res.send("User not authenticated");
-      }
-    } else {
-      res.send("User not authenticated");
-    }
 })
 
 app.listen(port, () => {
