32f4d2e8317ce756be79955261f4583ca5c9ebc3
Jualiche <SkettK@gmail.com>
2025-05-22 16:32:23 -0600

    Login created
    Login page was created and successfully connects with database

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,26 @@ const app = express()
 const port = 9000
 app.use(express.json())
 
+
+
 app.post('/auth', async(req, res) => {
   const { username, password } = req.body;
   if( !username || !password ) {
     res.status(400).send("Username and password are required");
     return;
   }
-   console.log('BODY:', req.body); 
+  console.log('BODY:', req.body); 
   const client = await connect();
-  const result = await query( 'SELECT * FROM users WHERE username = $1 AND password = crypt($2, password);', [username,password],client);
+  
+  const result = await query(
+    `SELECT u.id, u.name, u.rol 
+     FROM users u 
+     JOIN passwords p ON u.id = p.iduser 
+     WHERE u."user" = $1 AND p.password = crypt($2, p.password)`,
+    [username, password],
+    client
+  );
+  
   console.log('RESULT:', result.rows);
   if (result && result.rows.length > 0) {
     res.send("User authenticated");
